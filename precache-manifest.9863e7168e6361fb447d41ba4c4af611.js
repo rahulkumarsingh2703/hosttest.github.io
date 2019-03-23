1dd69c2990c57c2b73fd8532accd14c1004d219a
Rahul Kumar Singh <rsrajput.001@gmail.com>
2019-03-23 12:25:11 +0530

    Updates

---
+++ precache-manifest.9863e7168e6361fb447d41ba4c4af611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c27fe8476a8be912faa",
+    "url": "/hosttest/static/css/main.bc5ebb3c.chunk.css"
+  },
+  {
+    "revision": "2c27fe8476a8be912faa",
+    "url": "/hosttest/static/js/main.77e89215.chunk.js"
+  },
+  {
+    "revision": "018fd519b27bb8835339",
+    "url": "/hosttest/static/js/runtime~main.533501ac.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/hosttest/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hosttest/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e8b7cb1e1af8a4939831b23132dd952c",
+    "url": "/hosttest/index.html"
+  }
+];
